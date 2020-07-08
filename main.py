2844f1c8911c59859559634ab509440075d9644f
Peter Renshaw <peterrenshaw@seldomlogical.com>
2020-07-08 22:08:45 +1000

    2020JUL08 initial checkin

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# ~*~ coding: utf-8 ~*~
+
+
+from inky import InkyPHAT
+from PIL import Image, ImageFont, ImageDraw
+from font_untuitive import Intuitive
+
+
+scale_size = 1.0
+intuitive_font = ImageFont.truetype(Intuitive, int(22 * scale_size))
+
+

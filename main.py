3e5759250460ee46a0516f4b2db810270e805879
Peter Renshaw <peterrenshaw@seldomlogical.com>
2020-07-12 18:14:18 +1000

    2020JUL12 clean the code, README

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 
 #
 # name: main.py
-# date: 2020JUL08
+# date: 2020JUL12
+#       2020JUL08
 # prog: pr
 # desc: A simple pihole mini-hack to display
 #       the CRON update cycle onscreen.
@@ -24,27 +25,19 @@ from PIL import ImageDraw
 from inky import InkyPHAT
 
 
-#
-# setup
-#
-COLOR = "red"
-SCALE_SIZE = 1.0
-MMMDD = "%b%d"
-DT = "{}".format( strftime(MMMDD, localtime()).upper())
-ID = InkyPHAT(COLOR)
-PATH = os.path.dirname(__file__) 
-FONT = ImageFont.truetype("DejaVuSansMono.ttf", 30)
+DEBUG = False
 
 
+# disp: show debug messages
+def disp(msg, is_debug=DEBUG):
+    if is_debug: print("\t>{}".format(msg))
+    else: pass
 
 #
 # create_mask
 # < https://github.com/pimoroni/inky/blob/master/examples/phat/weather-phat.py>
 # 
-def create_mask(source,
-                mask=(ID.WHITE, 
-                      ID.BLACK,
-                      ID.RED)):
+def create_mask(source, mask):
     """given an image, create an appropriate mask to display image"""
     mask_image = Image.new("1", source.size)
     w, h = source.size
@@ -57,21 +50,28 @@ def create_mask(source,
     return mask_image
 
 
+disp("setup")
+COLOR = "red"
+SCALE_SIZE = 1.0
+MMMDD = "%b%d"
+DT = "{}".format( strftime(MMMDD, localtime()).upper())
+ID = InkyPHAT(COLOR)
+PATH = os.path.dirname(__file__)
+FONT = ImageFont.truetype("DejaVuSansMono.ttf", 30)
+disp("setup ok")
+
+
 #
-# main: main cli entry point
-# 
+# main
+#
 def main():
-    """main cli entry point"""
-    #
+    # setup
+
+    disp('start')
     # dictionaries to store icons and masks in
-    #
     icons = {}
     mask = {}
-
-
-    # 
     # maps icon to time phase
-    # 
     icon_map = {
         "first":  ["wind"],
         "second": ["sun"],
@@ -79,40 +79,41 @@ def main():
         "forth":  ["storm"]
     }
 
+    disp("icon mapp <{}>".format(icon_map))
     time_icon = None
     time_icon = icon_map["second"]
-    print("time_icon {}".format(time_icon))
-
+    disp("time icon <{}>".format(time_icon))
 
     # canvas
-    img = Image.open(os.path.join(PATH, "logoneek.png"))
+    disp("create image and draw")
+    img = Image.open(os.path.join(PATH,"img",  "logoneek.png"))
     draw = ImageDraw.Draw(img)
 
-
     # load icon files and masks
-    for icon in glob(os.path.join(PATH, "icon-*.png")):
-        icon_name = icon.split("icon-")[1].replace(".png", "") 
+    disp("load icons")
+    for icon in glob(os.path.join(PATH,"img",  "icon-*.png")):
+        icon_name = icon.split("icon-")[1].replace(".png", "")
         icon_image = Image.open(icon)
         icons[icon_name] = icon_image
-        mask[icon_name] = create_mask(icon_image)
+        mask[icon_name] = create_mask(icon_image,
+                                      (ID.WHITE,
+                                       ID.BLACK,
+                                       ID.RED))
 
     TI = time_icon[0]
-    print("time_icon        <{}>".format(TI))
-    print("icons[time_icon] <{}>".format(icons[TI]))
-
-    print("mask[time_icon]  <{}>".format(mask[TI]))
-
+    disp("time icon        <{}>".format(TI))
+    disp("icons[time_icon] <{}>".format(icons[TI]))
+    disp("mask[time_icon]  <{}>".format(mask[TI]))
     img.paste(icons[TI], (174, -10), mask[TI])
     ID.set_image(img)
     ID.show()
 
-
-#
-# main
-#
-if __name__ == "_main__":
-    main()
+    disp("fin")
  
+
+if __name__ == "__main__":
+    main()
+
 # eof  
 
 

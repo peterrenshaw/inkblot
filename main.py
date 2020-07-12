afc3cc49c95c82cb9193831c60edc3573e1de452
Peter Renshaw <peterrenshaw@seldomlogical.com>
2020-07-12 17:36:28 +1000

    2020JUL12 clean code, README

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ def create_mask(source,
                 mask=(ID.WHITE, 
                       ID.BLACK,
                       ID.RED)):
+    """given an image, create an appropriate mask to display image"""
     mask_image = Image.new("1", source.size)
     w, h = source.size
     for x in range(w):
@@ -57,49 +58,61 @@ def create_mask(source,
 
 
 #
-# dictionaries to store icons and masks in
+# main: main cli entry point
+# 
+def main():
+    """main cli entry point"""
+    #
+    # dictionaries to store icons and masks in
+    #
+    icons = {}
+    mask = {}
+
+
+    # 
+    # maps icon to time phase
+    # 
+    icon_map = {
+        "first":  ["wind"],
+        "second": ["sun"],
+        "third":  ["rain"],
+        "forth":  ["storm"]
+    }
+
+    time_icon = None
+    time_icon = icon_map["second"]
+    print("time_icon {}".format(time_icon))
+
+
+    # canvas
+    img = Image.open(os.path.join(PATH, "logoneek.png"))
+    draw = ImageDraw.Draw(img)
+
+
+    # load icon files and masks
+    for icon in glob(os.path.join(PATH, "icon-*.png")):
+        icon_name = icon.split("icon-")[1].replace(".png", "") 
+        icon_image = Image.open(icon)
+        icons[icon_name] = icon_image
+        mask[icon_name] = create_mask(icon_image)
+
+    TI = time_icon[0]
+    print("time_icon        <{}>".format(TI))
+    print("icons[time_icon] <{}>".format(icons[TI]))
+
+    print("mask[time_icon]  <{}>".format(mask[TI]))
+
+    img.paste(icons[TI], (174, -10), mask[TI])
+    ID.set_image(img)
+    ID.show()
+
+
 #
-icons = {}
-mask = {}
-
-
-# 
-# maps icon to time phase
-# 
-icon_map = {
-    "first":  ["wind"],
-    "second": ["sun"],
-    "third":  ["rain"],
-    "forth":  ["storm"]
-}
-time_icon = None
-time_icon = icon_map["second"]
-print("time_icon {}".format(time_icon))
-
-
-# canvas
-img = Image.open(os.path.join(PATH, "logoneek.png"))
-draw = ImageDraw.Draw(img)
-
-
-# load icon files and masks
-for icon in glob(os.path.join(PATH, "icon-*.png")):
-    icon_name = icon.split("icon-")[1].replace(".png", "") 
-    icon_image = Image.open(icon)
-    icons[icon_name] = icon_image
-    mask[icon_name] = create_mask(icon_image)
-
-TI = time_icon[0]
-print("time_icon        <{}>".format(TI))
-print("icons[time_icon] <{}>".format(icons[TI]))
-
-print("mask[time_icon]  <{}>".format(mask[TI]))
-
-img.paste(icons[TI], (174, -10), mask[TI])
-ID.set_image(img)
-ID.show()
-
-
+# main
+#
+if __name__ == "_main__":
+    main()
+ 
 # eof  
 
 
